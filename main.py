ce902fb30ce8be117f14737bb6b9c44b6bef4cc2
Bakasore <benjamin.garza03@gmail.com>
2026-04-12 19:14:19 -0700

    API can now take user input

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key is None:
-    raise RuntimeError(
-        "API Key not found. Please ensure 'MY_API_KEY' is set in your enviornment variables"
-    )
-
-client = genai.Client(api_key=api_key)
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError(
+            "API Key not found. Please ensure 'MY_API_KEY' is set in your environment variables"
+        )
+
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        model='gemini-2.5-flash', contents=messages
     )
+
+    if response.usage_metadata is None:
+        raise RuntimeError(
+            "No response from api."
+        )
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 if __name__ == "__main__":
     main()
